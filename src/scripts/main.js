9c30e5f183de5a8beaf5352276ba5de69571165d
Yuliia Babych <jl.babych29@gmail.com>
2025-04-23 15:11:26 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,33 @@
 'use strict';
 
 document.addEventListener('click', (e) => {
-  // write code here
+  e.preventDefault();
+
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
+
+  const wallRect = wall.getBoundingClientRect();
+
+  if (
+    e.clientX >= wallRect.left &&
+    e.clientX <= wallRect.right &&
+    e.clientY >= wallRect.top &&
+    e.clientY <= wallRect.bottom
+  ) {
+    const wallY = wall.clientHeight;
+    const wallX = wall.clientWidth;
+    const spiderX = spider.clientWidth;
+    const spiderY = spider.clientHeight;
+    const clickX = e.offsetX;
+    const clickY = e.offsetY;
+
+    const maxX = wallX - spiderX;
+    const maxY = wallY - spiderY;
+
+    spider.style.position = 'absolute';
+
+    spider.style.left =
+      Math.max(0, Math.min(clickX - spiderX / 2, maxX)) + 'px';
+    spider.style.top = Math.max(0, Math.min(clickY - spiderY / 2, maxY)) + 'px';
+  }
 });
